2de6474ee9517c1008cee462f4098e0f12d6b4cc
MX064 <david.pluto146@gmail.com>
2026-04-18 20:23:13 +0200

    good

--- vesselhunt/Assets/scripts/combat.cs
+++ vesselhunt/Assets/scripts/combat.cs
@@ -10,6 +10,8 @@ public class combat : MonoBehaviour
     [SerializeField] private GameObject goon2;
     [SerializeField] private GameObject goon3;
     [SerializeField] private GameObject goon4;              //declaring enemy game objects
+
+    [SerializeField] private GameObject Nyra;
     private float goon1x;
     private float goon1y;
     private float goon2y;
@@ -20,85 +22,129 @@ public class combat : MonoBehaviour
     private float goon4y;
     private float shadery;
     private float shaderx;
+    private float Nyray;
 
-    private int goon1health=40;                       //health for each goon, used in if statements below
-    private int goon2health=50;
-    private int goon3health=60;
-    private int goon4health=70;
+    private int goon1health=30;                       //health for each goon, used in if statements below
+    private int goon2health=40;
+    private int goon3health=50;
+    private int goon4health=60;
+    private int shaderhealth = 100;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
- 
-    }
 
     // Update is called once per frame
-    void Update()
+    void Start()
     {
         //make all goons invisble at start
         shadery = Shader.position.y;
         shaderx = Shader.position.x;
         goon1x = goon1.transform.position.x;
         goon1y = goon1.transform.position.y;
+        Nyray = Nyra.transform.position.y;
 
-        if (Math.Abs(shaderx-goon1x) < 5 && Math.Abs(shadery-goon1y) < 5)
-        {
-            //goon1 = visbile
-            Debug.Log("Press J to damage enemies");
-            if (Input.GetKey(KeyCode.J))
+        //add repeat loops ig
+
+        if (Nyray == -13.0)
+        {                                          //This ensure the player has spoken to Nyra first
+            goon1.GetComponent<SpriteRenderer>().color = Color.black;
+            if (Math.Abs(shaderx - goon1x) < 8 && Math.Abs(shadery - goon1y) < 8 && (goon1.GetComponent<SpriteRenderer>().color == Color.black))
             {
-                goon1health = goon1health - 10;
+                Debug.Log("Press J to damage enemies.Press H to heal");
+                if (Input.GetKey(KeyCode.J))
+                {
+                    goon1health = goon1health - 10;                          //goon attacks if Shader attacks
+                    shaderhealth = shaderhealth - 2;
+                    Debug.Log("Goon 1 health = " + goon1health + "Shader health = " + shaderhealth);
+                }
+                if (Input.GetKey(KeyCode.H))
+                {
+                    shaderhealth = shaderhealth + 5;                            //goon heals if Shader heals
+                    goon1health = goon1health + 1;
+                    Debug.Log("Goon 1 health = " + goon1health + "Shader health = " + shaderhealth);
+                }
             }
-        }
 
-        if (goon1health == 0)
-        {
-            //goon1 colour = red
-            //goon2 visible
-            //debug goon1 ded
-        }
-
-        goon2x = goon2.transform.position.x;
-        goon2y = goon2.transform.position.y;
-
-        if (Math.Abs(shaderx - goon2x) < 5 && Math.Abs(shadery - goon2y) < 5)
-        {
-            if (Input.GetKey(KeyCode.J))
+            if (goon1health == 0)
             {
-                goon2health = goon2health - 10;
+                goon1.GetComponent<SpriteRenderer>().color = Color.red;
+                goon2.GetComponent<SpriteRenderer>().color = Color.black;
+                Debug.Log("Defeat goon 2");
             }
-        }
 
-        if (goon2health == 0)
-        {
-            //goon2 colour = red
-            //goon3 visible
-        }
-        goon3x = goon3.transform.position.x;
-        goon3y = goon3.transform.position.y;
+            goon2x = goon2.transform.position.x;
+            goon2y = goon2.transform.position.y;
 
-        if (Math.Abs(shaderx - goon3x) < 5 && Math.Abs(shadery - goon3y) < 5)
-        {
-            if (Input.GetKey(KeyCode.J))
+            if (Math.Abs(shaderx - goon2x) < 2 && Math.Abs(shadery - goon2y) < 2 && (goon2.GetComponent<SpriteRenderer>().color == Color.black))
             {
-                goon3health = goon3health - 10;
+                if (Input.GetKey(KeyCode.J))
+                {
+                    goon2health = goon2health - 10;                          //goon attacks if Shader attacks
+                    shaderhealth = shaderhealth - 2;
+                    Debug.Log("Goon 2 health = " + goon2health + "Shader health = " + shaderhealth);
+                }
+                if (Input.GetKey(KeyCode.H))
+                {
+                    shaderhealth = shaderhealth + 5;                            //goon heals if Shader heals
+                    goon2health = goon2health + 1;
+                    Debug.Log("Goon 2 health = " + goon2health + "Shader health = " + shaderhealth);
+                }
             }
-        }
 
-        if (goon3health == 0)
-        {
-            //goon3 colour = red
-            //goon4 visible
-        }
-
-        goon4x = goon4.transform.position.x;
-        goon4y = goon4.transform.position.y;
-
-        if (Math.Abs(shaderx - goon4x) < 5 && Math.Abs(shadery - goon4y) < 5)
-        {
-            if (Input.GetKey(KeyCode.J))
+            if (goon2health == 0)
             {
-                goon4health = goon4health - 10;
+                goon2.GetComponent<SpriteRenderer>().color = Color.red;
+                goon3.GetComponent<SpriteRenderer>().color = Color.black;
+                Debug.Log("Defeat goon 3");
+            }
+            goon3x = goon3.transform.position.x;
+            goon3y = goon3.transform.position.y;
+
+            if (Math.Abs(shaderx - goon3x) < 2 && Math.Abs(shadery - goon3y) < 2 && (goon3.GetComponent<SpriteRenderer>().color == Color.black))
+            {
+                if (Input.GetKey(KeyCode.J))
+                {
+                    goon3health = goon3health - 10;                          //goon attacks if Shader attacks
+                    shaderhealth = shaderhealth - 2;
+                    Debug.Log("Goon 3 health = " + goon3health + "Shader health = " + shaderhealth);
+                }
+                if (Input.GetKey(KeyCode.H))
+                {
+                    shaderhealth = shaderhealth + 5;                            //goon heals if Shader heals
+                    goon3health = goon3health + 1;
+                    Debug.Log("Goon 3 health = " + goon3health + "Shader health = " + shaderhealth);
+                }
+            }
+
+            if (goon3health == 0)
+            {
+                goon3.GetComponent<SpriteRenderer>().color = Color.red;
+                goon4.GetComponent<SpriteRenderer>().color = Color.black;
+                Debug.Log("Defeat goon 4");
+            }
+
+            goon4x = goon4.transform.position.x;
+            goon4y = goon4.transform.position.y;
+
+            if (Math.Abs(shaderx - goon4x) < 2 && Math.Abs(shadery - goon4y) < 2 && (goon4.GetComponent<SpriteRenderer>().color == Color.black))
+            {
+                if (Input.GetKey(KeyCode.J))
+                {
+                    goon4health = goon4health - 10;                          //goon attacks if Shader attacks
+                    shaderhealth = shaderhealth - 2;
+                    Debug.Log("Goon 4 health = " + goon4health + "Shader health = " + shaderhealth);
+                }
+                if (Input.GetKey(KeyCode.H))
+                {
+                    shaderhealth = shaderhealth + 5;                            //goon heals if Shader heals
+                    goon4health = goon4health + 1;
+                    Debug.Log("Goon 4 health = " + goon4health + "Shader health = " + shaderhealth);
+                }
+            }
+
+            if (goon4health == 0)
+            {
+                goon4.GetComponent<SpriteRenderer>().color = Color.red;
+                Debug.Log("Goons all defeated. Level passed");
             }
         }
     }
